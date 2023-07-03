3c4c748f56e0c2600477c7d0d04860dc21261d13
Akshay Gupta <26359906+akshaygupta-dev@users.noreply.github.com>
2023-07-03 10:05:21 +0530

    removed extra code

--- ValidateJS.ps1
+++ ValidateJS.ps1
@@ -63,40 +63,4 @@ foreach ($line in $esLintOutputLogContent) {
         Get-Content -Path $esLintOutputLogContent
         exit 1
     }
-}$workingDir = "delta-changes"
-$currentDir = Get-Location
-$jsonLintOutputLogFile = Join-Path ${currentDir} "jsonlint_output.log"
-$staticResourceDir = Join-Path ${workingDir} "src\staticresources"
-$pattern = "<contentType>application/json</contentType>"
-$jsonDataFiles = @()
-
-if (Test-Path -Path "$staticResourceDir" -PathType Container) {
-    $staticResourceFiles = Get-ChildItem -Path "$staticResourceDir" -Filter "*.resource" -File -Recurse
-    if ($staticResourceFiles.Count -ne 0) {
-        foreach ($file in $staticResourceFiles) {
-            $fileName = (Get-Item -Path $(Join-Path ${staticResourceDir} ${file}) ).Name
-            $metaFile = Join-Path $staticResourceDir "${fileName}-meta.xml"
-            $match = Select-String -Path $metaFile -Pattern $pattern
-            if ($match) {
-                $jsonDataFiles += ($file)
-            }
-        }
-    }
-
-    if ($jsonDataFiles.Count -gt 0) {
-        Write-Host "Change(s) detected in JSON file(s) in Static Resource"
-
-        foreach ($file in $jsonDataFiles) {
-            Write-Host "Checking $($file.FullName)"
-            npx jsonlint $($file.FullName) >> $jsonLintOutputLogFile
-        }
-    }
-}
-
-$jsonLintOutputLogContent = Get-Content -Path $jsonLintOutputLogFile
-
-foreach ($line in $jsonLintOutputLogContent) {
-    if ($line -like "*error*" -or $line -like "*Error*") {
-        exit 1
-    }
 }
